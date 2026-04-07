fba8c3a8861edd833cbe01d687a842f7c738af0c
Hasitha20030117 <nawarathnaherath1971@gmail.com>
2026-04-07 15:57:10 +0530

    Remove multiple files

--- frontend/src/lib/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-	baseURL:"http://localhost:8070",
-	withCredentials: true, // send cookies to the server
-});
-
-export default axiosInstance;
